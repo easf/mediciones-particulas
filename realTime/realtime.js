c06c23530d46015eb37e0d75abd86c2548bd33a4
easf <esanchiz92@gmail.com>
2017-11-20 07:58:54 -0300

    fix bug in realtime

--- realTime/realtime.js
+++ realTime/realtime.js
@@ -17,55 +17,57 @@ app.engine('html', require('ejs').renderFile);
 
 app.use(express.static(__dirname + "/"));
 
+function calculate (res, lines) {
+
+    var currentLine = "";
+    var currentValue = "";
+    var cMedSum = 0;
+    var count = 0;
+
+    for (var i = lines.length - 1; i >= 0; i--) {
+        currentLine = lines[i];
+        if (currentLine.length > 0){
+            currentValue = currentLine.split(',').slice(-1)[0];
+                cMedSum = cMedSum + Number(currentValue);
+                count++;
+        }
+    }
+    cMedSum = cMedSum.toFixed(10);
+    
+    fs.readFile('factors', (err, factors) => {
+        if (err) throw err;    
+        var factorValues = factors.toString().split(',');
+        var corrFactor = Number(factorValues[0]);
+    
+        var cProm = cMedSum * corrFactor * 1000 / count;
+        var qIndexFactor = Number(factorValues[1]);
+        var ica = (cProm/qIndexFactor) * 100;
+        ica = ica.toFixed(10);   
+    
+        fs.readFile('apiKey', (err, apiKey) => {
+            if (err) throw err;    
+            res.render( 'pages/airStatus.html', {key: apiKey, value: ica});     
+        });
+    });	
+}
+
 app.get('/', function (req, res) { 
     var dateTime = new Date();
     var currentYear = dateTime.getFullYear();
     var fileName = 'mediciones/mediciones-' + currentYear + '.csv';
-
     readLastLine.read(fileName, REQUIREDROWS).then(function (lines) {
-        
         lines = lines.split('\n');
-        
         if (lines.length < REQUIREDROWS) {
             var previousYear = currentYear - 1;
             var previousFile = 'mediciones/mediciones-' + previousYear + '.csv';        
             readLastLine.read(previousFile, REQUIREDROWS - lines.length).then(function (remLines) {
                 remLines = remLines.split('\n');
-                
                 lines = lines.concat(remLines);
-                
-                var currentLine = "";
-                var currentValue = "";
-                var cMedSum = 0;
-                var count = 0;
-                for (var i = lines.length - 1; i >= 0; i--) {
-                    currentLine = lines[i];
-                    if (currentLine.length > 0){
-                        currentValue = currentLine.split(',').slice(-1)[0];
-                            cMedSum = cMedSum + Number(currentValue);
-                            count++;
-                    }
-                }
-                cMedSum = cMedSum.toFixed(10);
-                
-                fs.readFile('factors', (err, factors) => {
-                    if (err) throw err;    
-                    var factorValues = factors.toString().split(',');
-                    var corrFactor = Number(factorValues[0]);
-                
-                    var cProm = cMedSum * corrFactor * 1000 / count;
-                    var qIndexFactor = Number(factorValues[1]);
-                    var ica = (cProm/qIndexFactor) * 100;
-                    ica = ica.toFixed(10);   
-                
-                    fs.readFile('apiKey', (err, apiKey) => {
-                        if (err) throw err;    
-                        console.log("Hola");
-                        res.render( 'pages/airStatus.html', {key: apiKey, value: ica});     
-                    });
-                    
-                });
+ 				calculate(res, lines);               
+
             });
+        }else{
+        	calculate(res, lines);
         }
 
 
